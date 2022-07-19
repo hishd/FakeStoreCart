bea342aaba00342eff3aead01487ee9b2abe574b
Hishara Dilshan <hisharadilshan3@gmail.com>
2022-07-19 22:51:36 +0530

    Added Item Model

---
+++ FakeStoreCart/Data/Model/Item.swift
@@ -0,0 +1,23 @@
+//
+//  Item.swift
+//  FakeStoreCart
+//
+//  Created by Hishara Dilshan on 2022-07-19.
+//
+
+import Foundation
+
+struct Item: Decodable {
+    let id: String
+    let title: String
+    let price: Double
+    let description: String
+    let category: String
+    let image: String
+    let rating: Rating
+}
+
+struct Rating: Decodable {
+    let rate: Float
+    let count: Int
+}
