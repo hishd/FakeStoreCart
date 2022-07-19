09ab7f469cd8dd32ad9a51f02d9b8a21f79e032a
Hishara Dilshan <hisharadilshan3@gmail.com>
2022-07-19 22:47:25 +0530

    Added AppIcon Assets. Added HomeViewController.

---
+++ FakeStoreCart/Presentation/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  FakeStoreCart
+//
+//  Created by Hishara Dilshan on 2022-07-19.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+    }
+}
