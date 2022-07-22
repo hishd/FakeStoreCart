85f97ad874495d32fb632f0040d43d6f78940711
Hishara Dilshan <hisharadilshan3@gmail.com>
2022-07-23 00:28:49 +0530

    Added ItemInfoViewModel functions

--- FakeStoreCart/Presentation/ItemInfo/ItemInfoViewModel.swift
+++ FakeStoreCart/Presentation/ItemInfo/ItemInfoViewModel.swift
@@ -9,6 +9,14 @@ import Foundation
 import TinyCart
 
 class ItemInfoViewModel {
-    var item: Item?
+    let cart = Cart.shared
+    var item: Item
     
+    init(item: Item) {
+        self.item = item
+    }
+    
+    func addToCart(qty: Int) throws {
+        try cart.addItem(item: CartItem(name: item.title, price: item.price), qty: qty)
+    }
 }
